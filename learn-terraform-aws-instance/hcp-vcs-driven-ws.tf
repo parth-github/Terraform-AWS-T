a99ba0dec8ba5e5ec3746c66204e0fd01ad9ef6e
Smriti <linuxriti@gmail.com>
2025-06-07 17:37:08 +0530

    Remove cloud block

--- learn-terraform-aws-instance/hcp-vcs-driven-ws.tf
+++ learn-terraform-aws-instance/hcp-vcs-driven-ws.tf
@@ -1,14 +1,13 @@
 terraform {
-  /*
+  /** removed for vcs-driven
   cloud {
-   organization = "smriti-aws"
+    organization = "smriti-aws"
 
-   workspaces {
-     name = "Terraform-AWS-T"
-   }
+    workspaces {
+      name = "Terraform-AWS-T"
+    }
   }
-*/
-
+  */
 
   required_providers {
     aws = {
@@ -22,12 +21,14 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
+  #HCP TF Workspace Global environment variables of aws key and id added
+  ## OR
   #access_key = "my-access-key"
   #secret_key = "my-secret-key"
   #or
   #shared_config_files      = ["C:/Users/linux/.aws/config"]
-  shared_credentials_files = ["C:/Users/linux/.aws/credentials"]
-  profile                  = "smriti_pgm"
+  #shared_credentials_files = ["C:/Users/linux/.aws/credentials"]
+  #profile                  = "smriti_pgm"
 }
 
 resource "aws_instance" "app_server" {
